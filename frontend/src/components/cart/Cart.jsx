da18ef6a0176511b068090d2e28f1a3a4c10b601
wasim Haider55 <wasimhaider14379@gmail.com>
2023-10-02 10:06:58 +0500

    some changing

--- frontend/src/components/cart/Cart.jsx
+++ frontend/src/components/cart/Cart.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-const Cart = () => {
+const Cart = ({ product }) => {
+  const [quantity, setQuantity] = useState(1); // Initialize quantity with 1
+
+  // Function to handle quantity changes
+  const handleQuantityChange = (e) => {
+    const newQuantity = parseInt(e.target.value, 10);
+    setQuantity(newQuantity);
+  };
+
   return (
-    <div>Cart</div>
-  )
-}
+    <div>
+      <div>
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREAvfrlPSKwyKd2K0V_MP8hhXjh9boNfjThQ&usqp=CAU"
+          alt="title"
+        />
+      </div>
+      <div>
+        <p>Sun's Out, BOGO's Out</p>
+        <p>Through 8/26</p>
+        {/* <h1>Rs {product.price * quantity}</h1> */}
+        <label>
+          Quantity:
+          <input
+            type="number"
+            value={quantity}
+            onChange={handleQuantityChange}
+            min={1} // Set minimum value for quantity
+          />
+        </label>
+      </div>
+    </div>
+  );
+};
 
-export default Cart
+export default Cart;
